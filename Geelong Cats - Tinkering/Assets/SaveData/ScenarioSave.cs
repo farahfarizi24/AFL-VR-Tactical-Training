bc7526dd5e212b6cebfcc74ffdf905013efbb2d1
wangxinling <8239838@qq.com>
2022-07-22 17:13:17 +1000

    Testing save file

--- Geelong Cats - Tinkering/Assets/SaveData/ScenarioSave.cs
+++ Geelong Cats - Tinkering/Assets/SaveData/ScenarioSave.cs
@@ -22,12 +22,21 @@ public class ScenarioSave : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void FindAllPlayer()
+    {
+        var homePlayers = GameObject.FindGameObjectsWithTag("home");
+
+        foreach (var player in homePlayers)
+        {
+            Debug.Log(player.name);
+        }
     }
 }
