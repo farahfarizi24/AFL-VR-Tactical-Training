1e99de02cc6e3d71c3c4885975ad446692ae0e67
farahfarizi24 <61811820+farahfarizi24@users.noreply.github.com>
2023-06-26 19:25:53 +1000

    Remove since its giving error for panel hiding

--- Geelong Cats - Tinkering/Assets/_Scripts/LocalPlayerUIHides.cs
+++ Geelong Cats - Tinkering/Assets/_Scripts/LocalPlayerUIHides.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class LocalPlayerUIHides : MonoBehaviour
 {
-    public GameObject CoachPanel=null;
+    
     // Start is called before the first frame update
     void Start()
     {
-        CoachPanel = GameObject.FindGameObjectWithTag("CoachUI");
-        CoachPanel.SetActive(false);
+   
 
     }
 
